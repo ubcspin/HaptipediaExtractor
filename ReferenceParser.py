85401b44a6c930ed00a7d423ee31160270d8973d
John Sastrillo <john.sastrillo@gmail.com>
2018-05-11 17:04:37 -0700

    makes individuals, but can't remove forbidden char from title

--- ReferenceParser.py
+++ ReferenceParser.py
@@ -1,4 +1,5 @@
 import os
+import io
 import glob
 import string
 import xml.etree.ElementTree as ET
@@ -22,14 +23,13 @@ def parseReference(XMLFile):
 
     paper_title = next(root.iter("{http://www.tei-c.org/ns/1.0}title")).text
     if type(paper_title) is not unicode:
-        paper_title = unicode(paper_title, 'ascii')  #to ensure we are always working with unicode
+        paper_title = unicode(paper_title, 'ascii')  # to ensure we are always working with unicode
 
     if len(paper_title) > 150:
         paper_title = paper_title[:150]
-        paper_title = paper_title + "..."
+        paper_title = paper_title + "_"
 
     utf8_paper_title = paper_title.encode('ascii', 'ignore')
-    print "after-translation: " + utf8_paper_title
 
     if not os.path.exists(utf8_paper_title):  # should be later changed to name of the paper
         os.makedirs(utf8_paper_title)
@@ -40,33 +40,34 @@ def parseReference(XMLFile):
         os.makedirs('References')
 
     os.chdir('References')
-    #
-    # for biblStruct in root.iter("{http://www.tei-c.org/ns/1.0}biblStruct"):
-    #
-    #     if len(biblStruct.attrib.keys()) != 0:   # used to separate paper title from reference titles
-    #
-    #         ref = biblStruct.find("{http://www.tei-c.org/ns/1.0}analytic")
-    #
-    #         if ref is None:
-    #             # title is either in analytic element or monogr element
-    #             ref = biblStruct.find("{http://www.tei-c.org/ns/1.0}monogr")
-    #
-    #
-    #         title = ref.find("{http://www.tei-c.org/ns/1.0}title").text
-    #         print title
-    #
-    #
-    #         global count
-    #         count += 1
-    #
-    #         createRefFile(title, count)
+
+    for biblStruct in root.iter("{http://www.tei-c.org/ns/1.0}biblStruct"):
+
+        if len(biblStruct.attrib.keys()) != 0:   # used to separate paper title from reference titles
+
+            ref = biblStruct.find("{http://www.tei-c.org/ns/1.0}analytic")
+
+            if ref is None:
+                # title is either in analytic element or monogr element
+                ref = biblStruct.find("{http://www.tei-c.org/ns/1.0}monogr")
+
+            title = ref.find("{http://www.tei-c.org/ns/1.0}title").text
+
+            global count
+            count += 1
+
+            createRefFile(title, count)
+
+
+    os.chdir('..')
 
 
 def createRefFile(title, count):
 
-    with open("[" + str(count) + "] " + title + ".txt", 'w') as refFile:
-        refFile.write(("Title: " + title).encode('utf8'))
+    filename = "[" + str(count) + "].txt"
 
+    with open(filename, "w+") as refFile:
+        refFile.write("Title: " + (title).encode('utf8'))
 
 
 for file in glob.glob("*.xml"):
